198346b76726ae4474cdc36ecde7fbc3134621c9
Samuel Barobhuiya <sb4518@rit.edu>
2021-11-07 23:03:11 -0500

    Added all the files and my function (kind of works)

---
+++ Birdbath_Functions_for_Students/BirdBathFunc_CP_cls420.py
@@ -0,0 +1,146 @@
+#
+#   A problem function which uses intentionally obtuse variable names and almost no comments.
+#   The goal is for students to find the maximum of the function using gradient ascent,
+#   axially-aligned grid search, full grid search, or some combination of these techniques.
+#   CSCI-420 students who wish to try using Genetic Algorithms can try that too.
+#
+#   Dr. Thomas B. Kinsman
+#
+import math
+import numpy as np
+
+def urxyz( exes_parameter, why, zircon, rta, rtb, rtc ) :
+    bogart  = np.array( [ exes_parameter, why, zircon ] )
+    nu      = rta * (np.pi/180)
+    delta   = np.array( [ [1, 0, 0], [0, np.cos(nu), -np.sin(nu)], [0, np.sin(nu), np.cos(nu)] ] )
+    mu      = rtb * (np.pi/180);
+    unicorn = np.array( [ [np.cos(mu), 0, np.sin(mu)], [0, 1, 0], [-np.sin(mu), 0, np.cos(mu)] ] )
+    tu      = rtc * (np.pi/180);
+    iocane  = np.array( [[np.cos(tu), -np.sin(tu), 0], [np.sin(tu), np.cos(tu), 0], [0, 0, 1]] )
+    rq      = np.matmul( delta, np.matmul( unicorn, iocane ))
+    Pegasus = np.matmul( rq, bogart )
+    return Pegasus
+
+
+def BirdBathFunc_CP_cls420( Harry, Dumbledore, Sirius ) :
+    Snuffleupagus   = np.array( [ -204e-9, 20e-9, 427.854e-6, 999.87597e-3, 995.41971e-2  ] )
+    Susan           = np.array( [  0.96286, -0.65825, -0.83323 ] )
+    Ernie           = np.array( Harry )
+    Bob             = np.array( [  0.14605,  0.70891, -0.53299 ] )
+    Troll           = np.polyval( Snuffleupagus, Ernie )
+    Hooper          = np.array( [  0.22708, -0.25333, -0.14719 ] )
+    rot_tri         = urxyz( Susan, Bob, Hooper, Troll, Dumbledore, Sirius )
+    if ( rot_tri[2][0] < rot_tri[2][1] ) :
+         if ( rot_tri[2][0] < rot_tri[2][2] ) :
+             min_idx = 0
+         else :
+             min_idx = 2
+    else :
+         if ( rot_tri[2][1] < rot_tri[2][2] ) :
+             min_idx = 1
+         else :
+             min_idx = 2
+    minic                   = rot_tri[2][min_idx]
+    pradius                 = math.sqrt( 1 - (minic*minic) )
+    Hagrid                  = 1 - abs(rot_tri[2][min_idx]) 
+    Hedgewig                = ( math.pi * (Hagrid*Hagrid) / 3 ) * ( 3*1 - Hagrid )
+    Hermoine                = 4/3 * math.pi
+    Rous                    = Hedgewig / Hermoine 
+    GiantVariable           = Rous * Rous * 2   # Increase the sensitivity by squaring small quantity.
+    return GiantVariable
+
+
+if __name__ == '__main__' :
+    # Emit the answer, with sensitivity analysis:
+    ans = [  -1.205,  13.125,  -8.625 ]
+    nn  = BirdBathFunc_CP_cls420(  -1.205,  13.125,   -8.625 )
+    print('Fraction of Water = ', nn, '<-- THE BEST ANSWER\n' )
+
+
+    print('Sensitivity analysis for =/- 0.5 degrees:')
+    delta_deg = 1.0
+    nn  = BirdBathFunc_CP_cls420(   -1.205+delta_deg,  13.125,   -8.625 )
+    print('Fraction of Water with roll + delta_deg   = ', nn )
+    nn  = BirdBathFunc_CP_cls420(   -1.205-delta_deg,  13.125,   -8.625 )
+    print('Fraction of Water with roll - delta_deg   = ', nn )
+    nn  = BirdBathFunc_CP_cls420(   -1.205,  13.125+delta_deg,   -8.625 )
+    print('Fraction of Water with tilt + delta_deg   = ', nn )
+    nn  = BirdBathFunc_CP_cls420(   -1.205,  13.125-delta_deg,   -8.625 )
+    print('Fraction of Water with tilt - delta_deg   = ', nn )
+    nn  = BirdBathFunc_CP_cls420(   -1.205,  13.125,   -8.625+delta_deg )
+    print('Fraction of Water with twist+ delta_deg   = ', nn )
+    nn  = BirdBathFunc_CP_cls420(   -1.205,  13.125,   -8.625-delta_deg )
+    print('Fraction of Water with twist- delta_deg   = ', nn )
+
+    #  THIS IS AN EXAMPLE OF GRID SEARCH:
+    best_fract  = -100
+    min__parms  = [ -30.0, -30.0, -20.0 ]
+    max__parms  = [ +30.0, +30.0, +20.0 ]
+    best_parms  = [ -0.125, 0.125, 0.125 ]    # Small values near (0,0,0)
+    deg_inc     = 12 
+    while deg_inc > 0.00125 :
+
+        # Set the search ranges:
+        roll_min   = best_parms[0]-deg_inc*5
+        roll_max   = best_parms[0]+deg_inc*5
+        tilt_min   = best_parms[1]-deg_inc*5
+        tilt_max   = best_parms[1]+deg_inc*5
+        twst_min   = best_parms[2]-deg_inc*5
+        twst_max   = best_parms[2]+deg_inc*5
+
+        if ( roll_min <= min__parms[0] ) :
+            roll_min = min__parms[0]
+        if ( tilt_min <= min__parms[1] ) :
+            tilt_min = min__parms[1]
+        if ( twst_min <= min__parms[2] ) :
+            twst_min = min__parms[2]
+
+        if ( roll_max > max__parms[0] ) :
+            roll_max = max__parms[0]
+        if ( tilt_max > max__parms[1] ) :
+            tilt_max = max__parms[1]
+        if ( twst_max > max__parms[2] ) :
+            twst_max = max__parms[2]
+
+        # Initialize the search range:
+
+        #
+        # Run the actual grid search, with user feedback every <so many> iterations:
+        #
+        case_count = 1
+        for twst in np.arange( twst_min, twst_max+deg_inc, deg_inc ) :
+            case_count = case_count+1
+            if ( case_count == 10 ) :
+                case_count = 0
+                print( '...Testing with twist = ', twst );
+            for tilt in np.arange( tilt_min, tilt_max+deg_inc, deg_inc ) :
+                for roll in np.arange( roll_min, roll_max+deg_inc, deg_inc ) :
+                    fract = BirdBathFunc_CP_cls420(  roll, tilt, twst )
+                    if ( fract >= best_fract ) :
+                        best_fract = fract
+                        best_parms = [ roll, tilt, twst ]
+
+        # Shrink the size of the grid slowly:
+        deg_inc = (deg_inc * 15)/16;
+        
+    print('\nBest Value Found:' );
+    print('    Roll= ', best_parms[0], ' Tilt = ', best_parms[1], ' Twist = ', best_parms[2] )
+    print('    Fraction = ', best_fract  )
+
+    nn  = BirdBathFunc_CP_cls420(  -1.205,  13.125,   -8.625 )
+
+    print('NOMINAL ANSWER WAS:\n    Roll= ', ans[0], ' Tilt= ', ans[1], ' Twist = ', ans[2] );
+    print('    Fraction of Water =  ', nn, '  <-- NOMINAL expected best.\n' )
+
+
+
+
+    #  Emit a trial test case here, etc...:
+    print('\n\n\n')
+    nn  = BirdBathFunc_CP_cls420( -13.1250, 8.6250, 1.2051 )
+    print('Fraction of Water = ', nn, '<-- Example test case results\n' )
+
+
+
+    print('###########################################################################################')
+
---
+++ Sam_solution.py
@@ -0,0 +1,65 @@
+import BirdBathFunc_CC_cls420 as bbf
+import numpy as np
+
+if __name__ == "__main__":
+    range_extender = 5
+    best_fractional_volume = bbf.BirdBathFunc_CC_cls420( -31.055,  -9.375,   -5.875)
+
+    # The minimum and maximum range values for each parameter for the bird function
+    min_params = [-30.0, -30.0, -30.0]
+    max_params = [45.0, 45.0, 45.0]
+
+    # The best values for the range values.
+    # This will be changing each loop iteration in order to get the best values
+    # This will be done by reducing the range for search every loop
+    best_params = [-0.2, 0.2, 0.2]
+    # Initial degree increment value during each iteration
+    degree_increment = 10
+
+    # value to ensure that the loop doesn't go on for too long or plays with small values
+    while degree_increment > 0.5:
+        rotate_min = best_params[0] - (degree_increment * range_extender)
+        rotate_max = best_params[0] + (degree_increment * range_extender)
+        tilt_min = best_params[1] - (degree_increment * range_extender)
+        tilt_max = best_params[1] + (degree_increment * range_extender)
+        twist_min = best_params[2] - (degree_increment * range_extender)
+        twist_max = best_params[2] + (degree_increment * range_extender)
+
+        if rotate_min <= min_params[0]:
+            rotate_min = min_params[0]
+        if rotate_max <= max_params[0]:
+            rotate_max = max_params[0]
+        if tilt_min <= min_params[1]:
+            tilt_min = min_params[1]
+        if tilt_max <= max_params[1]:
+            tilt_max = max_params[1]
+        if twist_min <= min_params[2]:
+            twist_min = min_params[2]
+        if twist_max <= max_params[2]:
+            twist_max = max_params[2]
+
+        # A nested for loop of 3 fors that checks each value within the specified range
+        # for each parameter that is passed down in the Bird Bath Function.
+        count = 0
+        for rotate in np.arange(rotate_min, rotate_max, degree_increment):
+            count += 1
+            if count == 10:
+                count = 0
+            for tilt in np.arange(tilt_min, tilt_max, degree_increment):
+                for twist in np.arange(twist_min, twist_max, degree_increment):
+                    # Passes in the new parameters for the bird bath function to check the
+                    # Fraction of water that is left.
+                    fractional_volume = bbf.BirdBathFunc_CC_cls420(rotate, tilt, twist)
+                    if fractional_volume > best_fractional_volume:
+                        # stores the new best fractional volume and its corresponding parameters in the
+                        # best params list so that it could be used for the next main iteration.
+                        best_fractional_volume = fractional_volume
+                        best_params = [rotate, tilt, twist]
+                        print('BEST Fraction of Water = ', best_fractional_volume, '<-- Example test case results\n')
+                # print('ROTATE:', best_params[0], '\nTILT:', best_params[1], '\nTWIST:', best_params[2])
+                # print('Fraction of Water = ', fractional_volume, '<-- Example test case results' )
+
+        degree_increment = (degree_increment * 13) / 16
+    print('ROTATE:', best_params[0], '\nTILT:', best_params[1], '\nTWIST:', best_params[2])
+    print('Fraction of Water = ', best_fractional_volume, '<-- Example test case results\n')
+    print('###########################################################################################')
